2cd70a2564531600f26821c6c09ef59ba3f5f40a
Tarek-khwaiss <tarekonguitar@hotmail.com>
2022-12-28 20:33:19 +0200

    second api call successful

--- scripts/weather.js
+++ scripts/weather.js
@@ -11,10 +11,25 @@ const getLocationKey = async (cityName) => {
     const response = await fetch(baseString + query);
     const data = response.json();
 
+    // console.log(data[0]);
     return data;
 }
 
-const getForcast = async () => {
+const getForcast = async (cityKey) => {
+    // const key = '';
+    // getLocationKey(inputData)
+    //     .then((data) => {
+    //         // here we have to extract the key from the json object
+    //         key = data;
+    //     })
+    //     .catch('an error happened :/');
+    const baseString = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${cityKey}?apikey=${key}`;
+
+    const response = await fetch(baseString + query);
+    const data = await response.json();
+
+    return data[0];
 
 };
 
@@ -22,8 +37,14 @@ const getForcast = async () => {
 cityinput.addEventListener('submit', (e) => {
     e.preventDefault();
     inputData = cityinput.city.value;
-    getLocationKey(inputData)
-        .then((data) => { console.log(data) })
+    getLocationKey(inputData).then((data) => {
+        console.log(data[0]);
+        return getForcast(data[0].Key)
+            .then((data) => {
+                // function to change the html template and add the information
+                console.log(data);
+            })
+    })
         .catch('an error happened :/');
 });
 
