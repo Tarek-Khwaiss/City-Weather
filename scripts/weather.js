92dc227d63ae33cebd5acfe3ef8d92931fe4fc6c
Tarek-khwaiss <tarekonguitar@hotmail.com>
2022-12-28 16:55:08 +0200

    first api call successful and background image fixed

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,16 +1,30 @@
-const key = 'odVC1E35Qg6u837NeU95Jbt1D8wbSAca';
+const key = 'eiAAAcqE7f8iWXKSrYyzGpCV3T832Npq';
+const cityinput = document.querySelector('.location');
 
-const getLocationKey = async () => {
+
+const getLocationKey = async (cityName) => {
 
     const baseString = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-    const apiKey = `?apikey=${key}`;
+    const input = cityName;
+    const query = `?apikey=${key}&q=${input}`;
 
-    const response = await fetch('http://dataservice.accuweather.com/locations/v1/cities/search');
-    console.log(response.json());
+    const response = await fetch(baseString + query);
     const data = response.json();
+
     return data;
 }
 
-getCondition().then(() => {
-    console.log(data);
-}).catch('an error happened');
+const getForcast = async () => {
+
+};
+
+
+cityinput.addEventListener('submit', (e) => {
+    e.preventDefault();
+    inputData = cityinput.city.value;
+    getLocationKey(inputData)
+        .then((data) => { console.log(data) })
+        .catch('an error happened :/');
+});
+
+
