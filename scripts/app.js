c73c4886857ddb166fbcff0650bf71090761e0cc
Tarek-khwaiss <tarekonguitar@hotmail.com>
2023-01-02 15:50:58 +0200

    app fully functional

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,20 @@
 const card = document.querySelector('.card');
-const details = document.querySelector('.details')
+const details = document.querySelector('.details');
 
+const time = document.querySelector('.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
 
-    const CityDetails = data.cityDetails;
+    const cityDetails = data.cityDets;
     const weather = data.weather;
 
-    console.log(CityDetails);
+    console.log(cityDetails);
     console.log(weather);
 
     // changing the city details
     details.innerHTML = `<div class="text-center text-uppercase text-muted details">
-            <h5 id="cityName" class="my-3">${CityDetails.EnglishName}</h5>
+            <h5 id="cityName" class="my-3">${cityDetails.EnglishName}</h5>
             <div class="my-3">${weather.WeatherText}</div>
             <div class="display-4 my-4">
                 <span>${weather.Temperature.Metric.Value}</span>
@@ -23,13 +25,13 @@ const updateUI = (data) => {
     if (card.classList.contains('d-none'))
         card.classList.remove('d-none');
 
-    // data object is the weather object
-    if (weather.IsDayTime === true)
-        card.firstChild.innerHTML = `<img src="assets/sunny.gif" class="card-img-top weather-gif">`;
-    else {
-        console.log(card.firstChild);
-        card.firstChild.innerHTML = `<img src="assets/night_hill.gif" class="card-img-top weather-gif">`;
-    }
+
+    // changing the icon
+    icon.setAttribute('src', `assets/icons/${weather.WeatherIcon}.svg`);
+
+    let weatherImage = weather.IsDayTime ? 'assets/sunny.gif' : 'assets/night_hill.gif';
+    time.setAttribute('src', weatherImage);
+
 }
 
 const getWeather = async (input) => {
@@ -38,7 +40,7 @@ const getWeather = async (input) => {
     const weather = await getForcast(cityDetails[0].Key);
 
     // a weird behaviour when returning data[0] in the function getLocationKey, so I handled the index here
-    return { cityDetails: cityDetails[0], weather };
+    return { cityDets: cityDetails[0], weather };
 };
 
 cityinput.addEventListener('submit', (e) => {
